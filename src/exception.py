6dc1ec9528852cc1f79fba950f7e3bbc4b85e80d
unknown <ishumishra500@gmail.com>
2024-01-30 17:21:02 +0530

    Data Transformation Done

--- src/exception.py
+++ src/exception.py
@@ -1,24 +1,24 @@
-import sys
-import logging
+import sys  #to interact with python runtine environemnt to get details about the errors
+from src.logger import logging  #using logging function from logger file
 
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
+def error_message_detail(error, error_detail:sys):  #error detail parameter is used as sys object
+    _,_,exc_tb=error_detail.exc_info() #.exc_info function in sys module gives output as a tuple of type, value and traceback
+    file_name = exc_tb.tb_frame.f_code.co_filename  # to retrieve the filename where the error occured from the traceback object
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name, exc_tb.tb_lineno,str(error))
+        file_name, exc_tb.tb_lineno,str(error))  # writing the format of the error message
     
     return error_message
     
 
 
-class CustomeException(Exception):
-    def __init__(self,error_message, error_detail:sys):
+class CustomException(Exception):  #creating a CustomeException class and inherting properties from built in Exception class
+    def __init__(self,error_message, error_detail:sys): #special method in python class used to initiliaze the object's attribute or perform any setup required for the object
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
 
-    def __str__(self):
+    def __str__(self): #special method in python class used to convert an object into a string
         return self.error_message
     
 
--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,19 @@
-import logging
-import os
-from datetime import datetime
+import logging #to write logging messages
+import os #to navigate the file system and create a directory for logs
+from datetime import datetime #to use date and time for log messages
 
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"   # creating a log file filename for current time with the specified date format
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)  #creating a directory path where log files will be stored
+os.makedirs(logs_path,exist_ok=True)  #creating a separate directory for log files if it doesn't already exists.
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)  #creating the full path to the log file by joining logs path directory and log file filename.
 
+#configuring loging system with basic configuration
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    filename=LOG_FILE_PATH,  #specifying filename for the log file
+    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",  #specifying the format of the log message as timestamp, line number, logger name, log level, log message 
+    level=logging.INFO,  #This line sets the logging level to INFO, indicating that only log messages with a level of INFO or higher will be logged. 
 
 )
     
